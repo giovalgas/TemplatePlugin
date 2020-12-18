be8409a035d80bc57434412477f1611082c80226
Gio4E20 <37852106+Gio4E20@users.noreply.github.com>
2020-12-18 03:19:02 -0300

    Initial commit

---
+++ src/me/giodev/templatePlugin/TemplatePlugin.java
@@ -0,0 +1,12 @@
+package me.giodev.templatePlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TemplatePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        System.out.println("Plugin Started");
+    }
+
+}
